b1b86bbd3901ff63843c1f2439fafc39483da8b8
khlose <schwin60@hotmail.com>
2016-10-23 11:15:04 -0400

    Distance calculation applied

--- object tracking with coordinates.py
+++ object tracking with coordinates.py
@@ -17,7 +17,55 @@ args = vars(ap.parse_args())
 # ball in the HSV color space
 greenLower = (0, 100, 100)
 greenUpper = (20, 255, 255)
- 
+
+
+
+
+
+
+
+
+
+
+
+def find_marker(image):
+	# convert the image to grayscale, blur it, and detect edges
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (5, 5), 0)
+	edged = cv2.Canny(gray, 35, 125)
+
+	# find the contours in the edged image and keep the largest one;
+	# we'll assume that this is our piece of paper in the image
+	(_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+	c = max(cnts, key=cv2.contourArea)
+
+	cv2.drawContours(gray, cnts, -1, (0, 255, 0), 3)
+	# compute the bounding box of the of the paper region and return it
+	return cv2.minAreaRect(c)
+def distance_to_camera(knownWidth, focalLength, perWidth):
+	# compute and return the distance from the maker to the camera
+	return (knownWidth * focalLength) / perWidth
+
+# initialize the known distance from the camera to the object, which
+# in cm
+KNOWN_DISTANCE = 45.0
+# initialize the known object width, which in this case, the piece of
+# width (in cm)
+KNOWN_WIDTH = 27.9
+IMAGE_PATHS = ["image1.jpg","image2.jpg"]
+image = cv2.imread(IMAGE_PATHS[0])
+marker = find_marker(image)
+#print marker
+focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+print "focal length = ",focalLength
+
+
+
+
+
+
+
+
 # initialize the list of tracked points, the frame counter,
 # and the coordinate deltas
 pts = deque(maxlen=args["buffer"])
@@ -29,12 +77,36 @@ camera = cv2.VideoCapture(0)
 while True:
 	# grab the current frame
 	(grabbed, frame) = camera.read()
- 
+
 	# if we are viewing a video and we did not grab a frame,
 	# then we have reached the end of the video
 	if args.get("video") and not grabbed:
 		break
- 
+
+
+
+
+
+
+
+
+	marker = find_marker(frame)
+	perWidth = marker[1][0]
+	distance1 = distance_to_camera(KNOWN_WIDTH, focalLength, perWidth)
+	print distance1, "(in cm)"
+
+
+
+
+
+
+
+
+
+
+
+
+
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
 	frame = imutils.resize(frame, width=1000)
